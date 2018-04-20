e4e1c1efe7bf5c72288793915f75ea71db3e4daf
newtonstudio <newtonstudio@gmail.com>
2018-04-20 20:48:28 +0800

    add description and resize image

--- about.php
+++ about.php
@@ -9,6 +9,7 @@
 			<li><a href="about.php">ABOUT</a></li>
 		</ul>
 		<h1>About</h1>		
-		<p><img src="garfield.jpg"/></p>
+		<p><img src="garfield.jpg" height="300px"/></p>
+		<p>This is the greatest cat</p>
 	</body>
 </html>
