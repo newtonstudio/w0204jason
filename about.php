245e4fdb2d2e60d2a5cdd82789fdf8e0dee7a31f
newtonstudio <newtonstudio@gmail.com>
2018-04-20 21:03:36 +0800

    add contact.php

--- about.php
+++ about.php
@@ -7,6 +7,7 @@
 		<ul>
 			<li><a href="index.php">HOME</a></li>
 			<li><a href="about.php">ABOUT</a></li>
+			<li><a href="contact.php">CONTACT</a></li>
 		</ul>
 		<h1>About</h1>		
 		<p><img src="garfield.jpg" height="300px"/></p>
--- contact.php
+++ contact.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Home (My Sweet Shop)</title>
+		<title>About</title>
 	</head>
 	<body>
-
 		<ul>
 			<li><a href="index.php">HOME</a></li>
 			<li><a href="about.php">ABOUT</a></li>
 			<li><a href="contact.php">CONTACT</a></li>
 		</ul>
-		<h1>Home</h1>
-		<p>Welcome to our website</p>		
+		<h1>Contact</h1>		
 	</body>
 </html>
